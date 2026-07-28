cefd19648c2d43b43f705ee4cfbe540b402292fa
Granthj <dasgranth24@gmail.com>
2026-07-28 20:28:06 +0530

    Added neon and change some code in db.js

--- Backend/Utils/db.js
+++ Backend/Utils/db.js
@@ -3,14 +3,16 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 require('pg');
 require('pg-hstore');
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    logging: false,
+
     dialectOptions: {
-        ssl: { require: true, rejectUnauthorized: false }
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
 });
 
@@ -18,8 +20,7 @@ const sequelize = new Sequelize(
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-    } 
-    catch (error) {
+    } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
 })();
