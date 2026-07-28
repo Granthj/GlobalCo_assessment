d256efe2d81e7a5135c1f86c648b1e6b2dc6df92
Granthj <dasgranth24@gmail.com>
2026-07-28 19:50:47 +0530

    add debug step for env file using JSON stringy finding hidden spcaes or anything

--- Frontend/api/client.js
+++ Frontend/api/client.js
@@ -1,6 +1,6 @@
 // const BASE_URL = 'http://localhost:3000/api';
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-console.log('BASE_URL:', BASE_URL);
+console.log("DEBUG BASE_URL:", JSON.stringify(BASE_URL));
 export async function getJobs() {
   try {
     const res = await axios.get(`${BASE_URL}/jobs/get-jobs`);
